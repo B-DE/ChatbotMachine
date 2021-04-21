ec5668411c037fe1f0b265f21114b944dc9ba169
yunju-kang <daria0155@naver.com>
2021-04-21 17:05:42 +0900

    edit module setting

--- demo/application.py
+++ demo/application.py
@@ -11,8 +11,8 @@ from kochat.loss import CRFLoss, CosFace, CenterLoss, COCOLoss, CrossEntropyLoss
 from kochat.model import intent, embed, entity
 from kochat.proc import DistanceClassifier, GensimEmbedder, EntityRecognizer, SoftmaxClassifier
 
-from demo.scenario import dust, weather, travel, restaurant
-# from scenario import dust, weather, travel, restaurant
+# from demo.scenario import dust, weather, travel, restaurant
+from scenario import dust, weather, travel, restaurant
 # 에러 나면 이걸로 실행해보세요!
 
 # gui 오류에 대한 코드
